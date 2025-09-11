bd3ecadc04660b26996140b9d7f9e56463392428
Bhupendr Saahuu <saahuubhupendr@gmail.com>
2025-09-11 20:06:46 +0530

    Print array func added

--- Array/PrintArray.java
+++ Array/PrintArray.java
@@ -1,7 +1,11 @@
 package Array;
 
+import java.util.Arrays;
+
 public class PrintArray {
     public static void main(String[] args) {
+        int[] a = {1, 2, 3, 4};
         System.out.println("Hello world!");
+        System.out.println(Arrays.toString(a));
     }
 }
