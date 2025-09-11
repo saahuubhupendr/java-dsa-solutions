440cfdacf35c8819935b61ba7e5d58e3eab40d50
Bhupendr Saahuu <saahuubhupendr@gmail.com>
2025-09-11 19:59:07 +0530

    Initial commit

---
+++ String/ValidAnagram.java
@@ -0,0 +1,25 @@
+package String;
+
+import java.util.Arrays;
+
+public class ValidAnagram {
+    public static boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) return false;
+
+        char[] arr1 = s.toCharArray();
+        char[] arr2 = t.toCharArray();
+
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+
+        return Arrays.equals(arr1, arr2);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isAnagram("listen", "silent")); // true
+        System.out.println(isAnagram("rat", "car"));       // false
+        System.out.println(isAnagram("triangle", "integral")); // true
+        System.out.println(isAnagram("hello", "world"));   // false
+    }
+}
+
