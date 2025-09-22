676423233f5c08479e73030a9164667d03c252a9
Bhupendr Saahuu <saahuubhupendr@gmail.com>
2025-09-22 19:15:26 +0530

    New Solution added in Array file

---
+++ Array/IntersectionofTwoArrays.java
@@ -0,0 +1,36 @@
+package Array;
+
+import java.util.*;
+
+class Solution {
+    public int[] intersect(int[] nums1, int[] nums2) {
+        // Store frequencies of nums1
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int num : nums1) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+
+        List<Integer> result = new ArrayList<>();
+        for (int num : nums2) {
+            if (map.containsKey(num) && map.get(num) > 0) {
+                result.add(num);
+                map.put(num, map.get(num) - 1);  // reduce frequency
+            }
+        }
+
+        // Convert List to int[]
+        int[] ans = new int[result.size()];
+        for (int i = 0; i < result.size(); i++) {
+            ans[i] = result.get(i);
+        }
+        return ans;
+    }
+
+    // Testing
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        int[] nums1 = {1,2,2,1};
+        int[] nums2 = {2,2};
+        System.out.println(Arrays.toString(sol.intersect(nums1, nums2))); // [2, 2]
+    }
+}
