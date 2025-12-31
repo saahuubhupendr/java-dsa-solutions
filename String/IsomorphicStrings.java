400881beddc44fca4c47dfd1c297fa8a68b50216
saahuubhupendr <saahuubhupendr@gmail.com>
2025-12-31 22:07:53 +0530

    Update IsomorphicStrings.java

--- String/IsomorphicStrings.java
+++ String/IsomorphicStrings.java
@@ -2,7 +2,7 @@ package String;
 
 import java.util.HashMap; 
 
-public class IsomorphicStrings {
+public class IsomorphicStrings { 
     public static boolean isIsomorphic(String s, String t) {
         if (s.length() != t.length()) return false; 
 
