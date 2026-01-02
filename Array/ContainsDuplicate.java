906ee6aae097e28f8b5419a6ff7c4918aa9466c4
saahuubhupendr <saahuubhupendr@gmail.com>
2026-01-02 12:55:52 +0530

    Update ContainsDuplicate.java

--- Array/ContainsDuplicate.java
+++ Array/ContainsDuplicate.java
@@ -5,7 +5,7 @@ public class ContainsDuplicate {
         for (int i = 0; i < nums.length; i++) {
             for (int j = i + 1; j < nums.length; j++) {
                 if (nums[i] == nums[j]) {
-                    return true;
+                    return true; 
                 }
             }
         }
