3bf5938651a77967d16a0e4e471fa253c322e745
saahuubhupendr <saahuubhupendr@gmail.com>
2026-01-02 12:55:52 +0530

    Update ContainsDuplicate.java

---
+++ String/RansomNote.java
@@ -0,0 +1,31 @@
+package String;
+
+public class RansomNote {
+
+        public boolean canConstruct(String ransomNote, String magazine) {
+            int[] freq = new int[26];
+
+            for (char c : magazine.toCharArray()) {
+                freq[c - 'a']++;
+            }
+
+            for (char c : ransomNote.toCharArray()) {
+                if (freq[c - 'a'] == 0) {
+                    return false;
+                }
+                freq[c - 'a']--;
+            }
+
+            return true;
+        }
+
+        public static void main(String[] args) {
+            RansomNote sol = new RansomNote();
+
+            String ransomNote = "aa";
+            String magazine = "aab";
+
+            boolean result = sol.canConstruct(ransomNote, magazine);
+            System.out.println("Can construct ransom note: " + result);
+        }
+    }
