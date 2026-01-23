4f14530cc8fe2c4e5494e990f4e59564d00ed47b
saahuubhupendr <saahuubhupendr@gmail.com>
2026-01-23 11:11:33 +0530

    Update comment for clarity in PrintArray.java

--- Array/PrintArray.java
+++ Array/PrintArray.java
@@ -2,7 +2,7 @@ package Array;
 
 import java.util.Arrays;
 
-// print Array 
+// print Array
 public class PrintArray {
     public static void main(String[] args) {
         int[] a = {1, 2, 3, 4};
