9c32dd07e5af35bcb8923c9714f28892325af8d0
saahuubhupendr <saahuubhupendr@gmail.com>
2026-04-26 08:14:09 +0530

    Update ValidAnagram.java

--- String/ValidAnagram.java
+++ String/ValidAnagram.java
@@ -9,7 +9,7 @@ public class ValidAnagram {
         char[] arr1 = s.toCharArray();
         char[] arr2 = t.toCharArray();
 
-        Arrays.sort(arr1);
+        Arrays.sort(arr1); 
         Arrays.sort(arr2);
 
         return Arrays.equals(arr1, arr2);
