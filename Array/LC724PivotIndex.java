fada0221b32f17802f3fcd8f481ecd8c9479ed91
Bhupendr Saahuu <saahuubhupendr@gmail.com>
2026-06-05 12:00:00 +0530

    Solved LeetCode-724 Pivot Index

---
+++ Array/LC724PivotIndex.java
@@ -0,0 +1,40 @@
+package Array;
+
+public class LC724PivotIndex {
+
+    public static int pivotIndex(int[] nums) {
+
+        int totalSum = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+
+            totalSum += nums[i];
+        }
+
+        int leftSum = 0;
+        int rightSum = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+
+             rightSum = totalSum - leftSum - nums[i];
+
+             if (leftSum == rightSum) {
+
+                 return i;
+             }
+
+             leftSum += nums[i];
+
+
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums = {1, 7, 3, 6, 5, 6};
+
+        System.out.println(pivotIndex(nums));
+    }
+}
