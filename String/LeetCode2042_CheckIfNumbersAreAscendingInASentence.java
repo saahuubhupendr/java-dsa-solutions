04f35ce4466c663d2848b8429738691ab7e37f29
Bhupendr Saahuu <saahuubhupendr@gmail.com>
2026-06-07 19:07:57 +0530

    Solve LeetCode 2042 - Check if Numbers Are Ascending in a Sentence

---
+++ String/LeetCode2042_CheckIfNumbersAreAscendingInASentence.java
@@ -0,0 +1,34 @@
+package String;
+
+public class LeetCode2042_CheckIfNumbersAreAscendingInASentence {
+
+    public static boolean areNumbersAscending(String s) {
+
+        String[] words = s.split(" ");
+
+        int prev = -1;
+
+        for (int i = 0; i < words.length; i++) {
+
+            if (Character.isDigit(words[i].charAt(0))) {
+
+                int curr = Integer.parseInt(words[i]);
+
+                if (curr <= prev) {
+                    return false;
+                }
+
+                prev = curr;
+            }
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+        String s = "1 box has 3 blue 4 red 6 green and 12 yellow marbles";
+
+        System.out.println(areNumbersAscending(s));
+    }
+}
