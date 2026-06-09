0be82c7b14b344a92fed6f9353d94cd38eeda175
Bhupendr Saahuu <saahuubhupendr@gmail.com>
2026-06-09 17:18:36 +0530

    Solved LeetCode 771 - Jewels and Stones using nested loop character comparison

---
+++ String/LeetCode771_JewelsAndStones.java
@@ -0,0 +1,35 @@
+package String;
+
+public class LeetCode771_JewelsAndStones {
+
+    public int numJewelsInStones(String jewels, String stones) {
+
+        int count = 0;
+
+        for (int i = 0; i < jewels.length(); i++) {
+
+            char chJewels = jewels.charAt(i);
+
+            for (int j = 0; j < stones.length(); j++) {
+
+                char chStones = stones.charAt(j);
+
+                if (chJewels == chStones) {
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+
+    public static void main(String[] args) {
+
+        LeetCode771_JewelsAndStones obj = new LeetCode771_JewelsAndStones();
+
+        String jewels = "aA";
+        String stones = "aAAbbbb";
+
+        System.out.println(obj.numJewelsInStones(jewels, stones));
+    }
+}
