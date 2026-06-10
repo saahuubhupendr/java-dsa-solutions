609defb6ff91c30ab318e52151112091b06547b8
Bhupendr Saahuu <saahuubhupendr@gmail.com>
2026-06-10 21:34:59 +0530

    Solved LeetCode 191 - Number of 1 Bits using Bit Manipulation

---
+++ BitManipulation/LeetCode191_NumberOf1Bits.java
@@ -0,0 +1,29 @@
+package BitManipulation;
+
+public class LeetCode191_NumberOf1Bits {
+
+    public int hammingWeight(int n) {
+
+        int count = 0;
+
+        while (n != 0) {
+
+            count += (n & 1);
+
+            n >>>= 1;
+        }
+
+        return count;
+    }
+
+    public static void main(String[] args) {
+
+        int n = 11; // Binary: 1011
+
+        LeetCode191_NumberOf1Bits solution = new LeetCode191_NumberOf1Bits();
+
+        int result = solution.hammingWeight(n);
+
+        System.out.println("Number of 1 Bits: " + result);
+    }
+}
