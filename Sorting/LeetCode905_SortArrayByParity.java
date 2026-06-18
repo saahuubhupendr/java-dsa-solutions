38ff22b0f15bd50ba2ce02771e630ada1566d6ea
Bhupendr Saahuu <saahuubhupendr@gmail.com>
2026-06-18 17:44:16 +0530

    Solved LeetCode 905: Sort Array By Parity using Two Pointers

---
+++ Sorting/LeetCode905_SortArrayByParity.java
@@ -0,0 +1,44 @@
+package Sorting;
+
+import java.util.Arrays;
+
+public class LeetCode905_SortArrayByParity {
+
+    public int[] sortArrayByParity(int[] nums) {
+
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left < right) {
+
+            if (nums[left] % 2 == 0) {
+                left++;
+            }
+            else if (nums[right] % 2 != 0) {
+                right--;
+            }
+            else {
+                int temp = nums[left];
+                nums[left] = nums[right];
+                nums[right] = temp;
+
+                left++;
+                right--;
+            }
+        }
+
+        return nums;
+    }
+
+    public static void main(String[] args) {
+
+        LeetCode905_SortArrayByParity solution =
+                new LeetCode905_SortArrayByParity();
+
+        int[] nums = {3, 1, 2, 4};
+
+        int[] result = solution.sortArrayByParity(nums);
+
+        System.out.println(Arrays.toString(result));
+    }
+}
