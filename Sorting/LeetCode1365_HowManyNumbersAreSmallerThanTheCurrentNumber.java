3243d13c07c18cfde9f6fd400642bc74bce812fd
Bhupendr Saahuu <saahuubhupendr@gmail.com>
2026-06-19 23:31:51 +0530

    Solved LeetCode 1365 - How Many Numbers Are Smaller Than the Current Number using Brute Force (O(n²))

---
+++ Sorting/LeetCode1365_HowManyNumbersAreSmallerThanTheCurrentNumber.java
@@ -0,0 +1,38 @@
+package Sorting;
+
+import java.util.Arrays;
+
+public class LeetCode1365_HowManyNumbersAreSmallerThanTheCurrentNumber {
+
+    public static int[] smallerNumbersThanCurrent(int[] nums) {
+
+        int[] ans = new int[nums.length];
+
+        for (int i = 0; i < nums.length; i++) {
+
+            int count = 0;
+
+            for (int j = 0; j < nums.length; j++) {
+
+                if (nums[j] < nums[i]) {
+                    count++;
+                }
+            }
+
+            ans[i] = count;
+        }
+
+        return ans;
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums1 = {8, 1, 2, 2, 3};
+        int[] nums2 = {6, 5, 4, 8};
+        int[] nums3 = {7, 7, 7, 7};
+
+        System.out.println(Arrays.toString(smallerNumbersThanCurrent(nums1)));
+        System.out.println(Arrays.toString(smallerNumbersThanCurrent(nums2)));
+        System.out.println(Arrays.toString(smallerNumbersThanCurrent(nums3)));
+    }
+}
