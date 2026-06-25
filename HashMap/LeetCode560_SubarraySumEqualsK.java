155980122dbf87b6d0a75cc2a097f413101ed92d
Bhupendr Saahuu <saahuubhupendr@gmail.com>
2026-06-25 22:11:03 +0530

    Solved LeetCode 560 - Subarray Sum Equals K using Prefix Sum and HashMap

---
+++ HashMap/LeetCode560_SubarraySumEqualsK.java
@@ -0,0 +1,38 @@
+package hashmap;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LeetCode560_SubarraySumEqualsK {
+
+    static int subarraySum(int[] nums, int k) {
+
+        int count = 0;
+        int sum = 0;
+
+        Map<Integer, Integer> map = new HashMap<>();
+
+        map.put(0, 1);
+
+        for (int num : nums) {
+
+            sum += num;
+
+            if (map.containsKey(sum - k)) {
+                count += map.get(sum - k);
+            }
+
+            map.put(sum, map.getOrDefault(sum, 0) + 1);
+        }
+
+        return count;
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums = {1, 1, 1};
+        int k = 2;
+
+        System.out.println("Number of subarrays = " + subarraySum(nums, k));
+    }
+}
