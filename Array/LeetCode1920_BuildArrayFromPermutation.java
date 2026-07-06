2df1bc9fbad2eb0567e6bd2b176bc41c493b6601
Bhupendr Saahuu <saahuubhupendr@gmail.com>
2026-07-06 21:20:00 +0530

    Solved Leetcode 1920 - Build Array from Permutation using Java

---
+++ Array/LeetCode1920_BuildArrayFromPermutation.java
@@ -0,0 +1,31 @@
+package Array;
+
+public class LeetCode1920_BuildArrayFromPermutation {
+
+    public int[] buildArray(int[] nums) {
+
+        int n = nums.length;
+        int[] ans = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            ans[i] = nums[nums[i]];
+        }
+
+        return ans;
+    }
+
+    public static void main(String[] args) {
+
+        LeetCode1920_BuildArrayFromPermutation solution = new LeetCode1920_BuildArrayFromPermutation();
+
+        int[] nums = {0, 2, 1, 5, 3, 4};
+
+        int[] result = solution.buildArray(nums);
+
+        System.out.print("Output: ");
+
+        for (int num : result) {
+            System.out.print(num + " ");
+        }
+    }
+}
