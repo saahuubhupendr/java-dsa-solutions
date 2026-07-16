f7938ac5f3d028ca7f92709fe5f1e0c28a77a8d8
Bhupendra <bhupendrasahu732@gmail.com>
2026-07-16 12:00:09 +0530

    Solved LeetCode 202 - Happy Number using Floyd's Cycle Detection (O(1) Space)

---
+++ String/LeetCode202_HappyNumber.java
@@ -0,0 +1,38 @@
+package String;
+
+public class LeetCode202_HappyNumber {
+
+    public static boolean isHappy(int n) {
+        int slow = n;
+        int fast = n;
+
+        do {
+            slow = getNext(slow);
+            fast = getNext(getNext(fast));
+        } while (slow != fast);
+
+        return slow == 1;
+    }
+
+    private static int getNext(int n) {
+        int sum = 0;
+
+        while (n > 0) {
+            int digit = n % 10;
+            sum += digit * digit;
+            n /= 10;
+        }
+
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        int n = 19;
+
+        if (isHappy(n)) {
+            System.out.println(n + " is a Happy Number.");
+        } else {
+            System.out.println(n + " is not a Happy Number.");
+        }
+    }
+}
