a1442b9392b87490d121742f02acc35844b3f775
Bhupendra <bhupendrasahu732@gmail.com>
2026-08-04 21:09:25 +0530

    Solved LeetCode 73 - Set Matrix Zeroes using row and column marker arrays (O(m+n) extra space)

---
+++ Array/LeetCode73SetMatrixZeroes.java
@@ -0,0 +1,61 @@
+package Array;
+
+import java.util.Arrays;
+
+public class LeetCode73SetMatrixZeroes {
+
+    public static void setZeroes(int[][] matrix) {
+
+        int m = matrix.length;
+        int n = matrix[0].length;
+
+        boolean[] rows = new boolean[m];
+        boolean[] cols = new boolean[n];
+
+        // Pass 1: Mark rows and columns containing zero
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+
+                if (matrix[i][j] == 0) {
+                    rows[i] = true;
+                    cols[j] = true;
+                }
+            }
+        }
+
+        // Pass 2: Set elements to zero
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+
+                if (rows[i] || cols[j]) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+
+    // Helper method to print matrix
+    public static void printMatrix(int[][] matrix) {
+        for (int[] row : matrix) {
+            System.out.println(Arrays.toString(row));
+        }
+    }
+
+    // Run the program
+    public static void main(String[] args) {
+
+        int[][] matrix = {
+                {1, 1, 1},
+                {1, 0, 1},
+                {1, 1, 1}
+        };
+
+        System.out.println("Original Matrix:");
+        printMatrix(matrix);
+
+        setZeroes(matrix);
+
+        System.out.println("\nMatrix After Set Zeroes:");
+        printMatrix(matrix);
+    }
+}
